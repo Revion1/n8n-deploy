c898d27bf131d56f6db60a9dbf3aa35088ece1f9
Revion1 <hajoumishak5@gmail.com>
2025-08-21 13:42:55 +0100

    Add files via upload

--- executions.ts
+++ executions.ts
@@ -1,46 +1,133 @@
-/**
- * Getters
- */
+import { stringify } from 'flatted';
+import pick from 'lodash/pick';
+import type {
+	IDataObject,
+	IRunData,
+	IRunExecutionData,
+	ITaskData,
+	ITaskDataConnections,
+} from 'n8n-workflow';
+import { nanoid } from 'nanoid';
 
-export const getExecutionsSidebar = () => cy.getByTestId('executions-sidebar');
+import { clickExecuteWorkflowButton } from '../composables/workflow';
 
-export const getWorkflowExecutionPreviewIframe = () => cy.getByTestId('workflow-preview-iframe');
+export function createMockNodeExecutionData(
+	name: string,
+	{
+		data,
+		inputOverride,
+		executionStatus = 'success',
+		jsonData,
+		...rest
+	}: Partial<ITaskData> & { jsonData?: Record<string, IDataObject> },
+): Record<string, ITaskData> {
+	return {
+		[name]: {
+			startTime: Date.now(),
+			executionIndex: 0,
+			executionTime: 1,
+			executionStatus,
+			data: jsonData
+				? Object.keys(jsonData).reduce((acc, key) => {
+						acc[key] = [
+							[
+								{
+									json: jsonData[key],
+									pairedItem: { item: 0 },
+								},
+							],
+						];
 
-export const getExecutionPreviewBody = () =>
-	getWorkflowExecutionPreviewIframe()
-		.its('0.contentDocument.body')
-		.should((body) => {
-			expect(body.querySelector('[data-test-id="canvas-wrapper"]')).to.exist;
-		})
-		.then((el) => cy.wrap(el));
-
-export const getExecutionPreviewBodyNodes = () =>
-	getExecutionPreviewBody().findChildByTestId('canvas-node');
-
-export const getExecutionPreviewBodyNodesByName = (name: string) =>
-	getExecutionPreviewBody().findChildByTestId('canvas-node').filter(`[data-name="${name}"]`).eq(0);
-
-export function getExecutionPreviewOutputPanelRelatedExecutionLink() {
-	return getExecutionPreviewBody().findChildByTestId('related-execution-link');
+						return acc;
+					}, {} as ITaskDataConnections)
+				: data,
+			source: [null],
+			inputOverride,
+			...rest,
+		},
+	};
 }
 
-export function getLogsOverviewStatus() {
-	return getExecutionPreviewBody().findChildByTestId('logs-overview-status');
+export function runMockWorkflowExecution({
+	trigger,
+	lastNodeExecuted,
+	runData,
+}: {
+	trigger?: () => void;
+	lastNodeExecuted: string;
+	runData: Array<ReturnType<typeof createMockNodeExecutionData>>;
+}) {
+	const workflowId = nanoid();
+	const executionId = Math.floor(Math.random() * 1_000_000).toString();
+
+	const resolvedRunData = runData.reduce<IRunData>((acc, nodeExecution) => {
+		const nodeName = Object.keys(nodeExecution)[0];
+		acc[nodeName] = [nodeExecution[nodeName]];
+		return acc;
+	}, {});
+
+	const executionData: IRunExecutionData = {
+		startData: {},
+		resultData: {
+			runData: resolvedRunData,
+			pinData: {},
+			lastNodeExecuted,
+		},
+		executionData: {
+			contextData: {},
+			nodeExecutionStack: [],
+			metadata: {},
+			waitingExecution: {},
+			waitingExecutionSource: {},
+		},
+	};
+
+	cy.intercept('POST', '/rest/workflows/**/run?**', {
+		statusCode: 201,
+		body: {
+			data: {
+				executionId,
+			},
+		},
+	}).as('runWorkflow');
+
+	if (trigger) {
+		trigger();
+	} else {
+		clickExecuteWorkflowButton();
+	}
+
+	cy.wait('@runWorkflow');
+
+	cy.push('executionStarted', {
+		workflowId,
+		executionId,
+		mode: 'manual',
+		startedAt: new Date(),
+		workflowName: '',
+		flattedRunData: '',
+	});
+
+	runData.forEach((nodeExecution) => {
+		const nodeName = Object.keys(nodeExecution)[0];
+		const nodeRunData = nodeExecution[nodeName];
+
+		cy.push('nodeExecuteBefore', {
+			executionId,
+			nodeName,
+			data: pick(nodeRunData, ['startTime', 'executionIndex', 'source', 'hints']),
+		});
+		cy.push('nodeExecuteAfter', {
+			executionId,
+			nodeName,
+			data: nodeRunData,
+		});
+	});
+
+	cy.push('executionFinished', {
+		executionId,
+		workflowId,
+		status: 'success',
+		rawData: stringify(executionData),
+	});
 }
-
-export function getLogEntries() {
-	return getExecutionPreviewBody().findChildByTestId('logs-overview-body').find('[role=treeitem]');
-}
-
-export function getManualChatMessages() {
-	return getExecutionPreviewBody().find('.chat-messages-list .chat-message');
-}
-
-/**
- * Actions
- */
-
-export const openExecutionPreviewNode = (name: string) =>
-	getExecutionPreviewBodyNodesByName(name).dblclick();
-
-export const toggleAutoRefresh = () => cy.getByTestId('auto-refresh-checkbox').click();
